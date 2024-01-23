6408fc6e6cac4b7f389d4e8491ccaca1c641fdaa
emzet93 <maciej.m.janawa@gmail.com>
2024-01-23 17:46:53 +0100

    use proper url for media

--- src/types.ts
+++ src/types.ts
@@ -19,6 +19,7 @@ export interface IArtistProfile {
   name: string;
   createdAtTime: string;
   avatarUrl?: string;
+  avatarIpfsHash?: string;
   websiteUrl?: string;
 }
 
--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,2 +1,15 @@
-export const getResizedArtworkUrl = (originalUrl: string, width: number) =>
-  `${originalUrl}?img-width=${width}&img-fit=cover`;
+const setTransformationsOnCloudinaryUrl = (
+  url: string,
+  transformationString: string,
+) => {
+  if (!url.includes('upload')) {
+    return url;
+  }
+
+  const [prefix, suffix] = url.split('/upload/');
+  return `${prefix}/upload/${transformationString}/${suffix}`;
+};
+
+export const getResizedArtworkUrl = (originalUrl: string, width: number) => {
+  return setTransformationsOnCloudinaryUrl(originalUrl, `c_fill,w_${width.toString()}`);
+};
